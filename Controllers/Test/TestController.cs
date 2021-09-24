9f11d30ef5cdf7ac5ce9bf61c01178d7c62bf803
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-09-24 09:48:16 +0545

    Test endpoint added for testing jwt token

---
+++ Controllers/Test/TestController.cs
@@ -0,0 +1,57 @@
+﻿using MeroBolee.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MeroBolee.Controllers.Environment
+{
+
+    [ApiController]
+   
+    public class TestController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
+
+        public TestController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        
+        [Route("GetEnvironment")]
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(environment.EnvironmentName);
+            }
+            catch (Exception ex)
+            {
+                return Ok("Error Message: " + ex.Message);
+                //turn StatusCode(500, ex);
+            }
+        }
+
+
+        [HttpGet("TestToken")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult TestJwtTokenAuthentication()
+        {
+            try
+            {
+                UserEntity account = HttpContext.Items["Account"] as UserEntity;
+                return Ok($"Token belongs to : {account.Person_email}");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+            }
+        }
+    }
+}
