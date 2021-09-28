e41d9ae67848147c9ff5f922f9b02687230c5d76
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-09-28 13:04:45 +0545

    CorrespondenceEmail entity added along with Repository

---
+++ Model/CorrespondenceEmailEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeroBolee.Model
+{
+    [Table("mb_correspondence_email")]
+    public class CorrespondenceEmailEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TenderId { get; set; }
+
+        public string MailBody { get; set; }
+        public string[] Attachments { get; set; }
+    }
+}
---
+++ Repository/Correspondence/CorrespondenceRepository.cs
@@ -0,0 +1,27 @@
+﻿using MeroBolee.Infrastructure;
+using MeroBolee.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeroBolee.Repository
+{
+    public interface ICorrespondenceRepository : IRepositoryBase<CorrespondenceEmailEntity>
+    {
+        public bool SendCorrespondenceEmail(CorrespondenceEmailEntity obj);
+    }
+    public class CorrespondenceRepository : RepositoryBase<CorrespondenceEmailEntity>, ICorrespondenceRepository
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public CorrespondenceRepository(IDbFactory dbFactory, IUnitOfWork unitOfWork) : base(dbFactory)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public bool SendCorrespondenceEmail(CorrespondenceEmailEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
