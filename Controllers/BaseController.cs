182c24b845e3adfbdd799e7e2c575f2c4583ea3b
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-11-01 20:43:16 +0545

    base controller added for user authorization

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeroBolee.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var item = HttpContext.Items["Account"];
+            base.OnActionExecuting(context);
+        }
+
+    }
+
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class AuthorizedBaseController: BaseController
+    {
+
+    }
+}
