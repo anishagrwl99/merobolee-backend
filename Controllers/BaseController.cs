416d42730aab21830ca6dbece4b8621fd3c9d339
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-11-02 13:06:51 +0545

    token user status and company status checked before initiating any request. if invalid 403 is returned

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,28 +1,43 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using MeroBolee.Dto;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MeroBolee.Controllers
 {
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+
     public class BaseController : Controller
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            var item = HttpContext.Items["Account"];
+            AuthenticateResponse user = HttpContext.Items["RequestUser"] as AuthenticateResponse;
+            //return StatusCode(StatusCodes.Status401Unauthorized);
+            bool NotExecuteAction = false;
+            if (user.UserStatusId != 1 || user.CompanyStatusId != 4)
+            {
+                context.HttpContext.Response.StatusCode = 403;
+                NotExecuteAction = true;
+            }
+
+            if (NotExecuteAction)
+            {
+                context.Result = Forbid();
+            }
+
             base.OnActionExecuting(context);
         }
 
-    }
-
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class AuthorizedBaseController: BaseController
-    {
 
     }
 }
