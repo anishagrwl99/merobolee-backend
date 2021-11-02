f5d2963267b235d1dc84ab0ff4ce740808492a0b
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-11-02 13:05:22 +0545

    jwt token validated and token user passed in request body to business rule validation

--- Middleware/JwtMiddleware.cs
+++ Middleware/JwtMiddleware.cs
@@ -1,3 +1,4 @@
+using MeroBolee.Dto;
 using MeroBolee.Model;
 using MeroBolee.Settings;
 using MeroBolee.Utility;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
@@ -39,7 +41,7 @@ namespace MeroBolee.Middleware
 
             if (token != null)
             {
-                bool isTokenValid = await attachAccountToContext(context, dataContext, token);
+                bool isTokenValid = AttachAccountToContext(context, dataContext, token);
 
                 if (isTokenValid)
                 {
@@ -61,41 +63,78 @@ namespace MeroBolee.Middleware
 
         }
 
-        private async Task<bool> attachAccountToContext(HttpContext context, MeroBoleeDbContext dataContext, string token)
+        /// <summary>
+        /// Attach token issued user to httpcontext for more business rule validation
+        /// </summary>
+        /// <param name="context">HTTP context on which request is being run</param>
+        /// <param name="dataContext">A database context for data flow</param>
+        /// <param name="token">A jwt token issued for a user during authentication</param>
+        /// <returns></returns>
+        private bool AttachAccountToContext(HttpContext context, MeroBoleeDbContext dataContext, string token)
         {
             try
             {
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
-                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                if (!context.Request.Path.StartsWithSegments("/Authenticate"))
                 {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidIssuer = _jwtSettings.Issuer,
-                    ValidAudience = _jwtSettings.Issuer,
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
-                    ClockSkew = TimeSpan.Zero
-                }, out SecurityToken validatedToken);
+                    // context.Request.HttpContext.User.Identity
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+                    tokenHandler.ValidateToken(token, new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidIssuer = _jwtSettings.Issuer,
+                        ValidAudience = _jwtSettings.Issuer,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                        ClockSkew = TimeSpan.Zero
+                    }, out SecurityToken validatedToken);
 
-                var jwtToken = (JwtSecurityToken)validatedToken;
-                int accountId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+                    var jwtToken = (JwtSecurityToken)validatedToken;
+                    //int accountId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+                    AuthenticateResponse requestUser = JsonConvert.DeserializeObject<AuthenticateResponse>(jwtToken.Claims.First(x => x.Type == "user").Value);
 
-                // attach account to context on successful jwt validation
-                UserEntity user=null;
-                string _cacheKey = $"{CacheKeys.LoginUser}_{accountId}";
-                if (!cache.TryGetValue<UserEntity>(_cacheKey, out user))
-                {
-                    user = await dataContext.UserEntities.FindAsync(accountId);
-                    cache.Set<UserEntity>(_cacheKey, user);
+                    // attach account to context on successful jwt validation
+                    //TokenUser user = null;
+                    //string _cacheKey = $"{CacheKeys.LoginUser}_{accountId}";
+                    //if (!cache.TryGetValue<TokenUser>(_cacheKey, out user))
+                    //{
+                    //    //user = await dataContext.UserEntities.FindAsync(accountId);
+
+                    //    user = (from u in dataContext.UserEntities
+                    //            join uc in dataContext.UserCompanies on u.User_Id equals uc.UserId
+                    //            join c in dataContext.CompanyEntities on uc.CompanyId equals c.CompanyId
+                    //            join r in dataContext.RoleEntities on u.Role_Id equals r.Role_Id
+                    //            where u.User_Id == accountId
+                    //            select new TokenUser
+                    //            {
+                    //                UserId = u.User_Id,
+                    //                UserStatusId = u.Status_id.Value,
+                    //                CompanyId = c.CompanyId,
+                    //                FirstName = u.First_Name,
+                    //                MiddleName = u.Middle_Name,
+                    //                LastName = u.Last_Name,
+                    //                Email = u.Person_email,
+                    //                AssignedRole = r.Role_Name,
+                    //                CompanyName = c.Name,
+                    //                CompanyEmail = c.CompanyEmail,
+                    //                CompanyCode = c.ReferenceCode,
+                    //                CompanyStatusId = c.CompanyStatusId.Value
+                    //            }
+                    //        ).FirstOrDefault();
+
+                    //    cache.Set<TokenUser>(_cacheKey, user);
+                    //}
+                    context.Items["RequestUser"] = requestUser;
+                    return true;
                 }
-                context.Items["Account"] = user;
                 return true;
             }
             catch (Exception)
             {
-                throw;
+                return false;
+                //throw;
                 // do nothing if jwt validation fails
                 // account is not attached to context so request won't have access to secure routes
             }
