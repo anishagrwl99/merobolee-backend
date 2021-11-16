8823963ac0f126f0dc319ba431399f76f3b776ed
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-11-16 15:31:23 +0545

    app default values settings added

---
+++ Settings/AppDefaults.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeroBolee.Settings
+{
+    public class AppDefaults
+    {
+        public string DefaultProfilePicture { get; set; }
+    }
+}
