e2e4effc5f1852a77488a3ebf3e98bc7c00f5af4
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2021-11-16 11:08:39 +0545

    delete file method added and implemented

--- Infrastructure/UploadImage.cs
+++ Infrastructure/UploadImage.cs
@@ -11,14 +11,19 @@ namespace MeroBolee.Infrastructure
     public interface IUploadFile
     {
         Task<string> Upload(IFormFile file, string folderName);
+        Task<bool> DeleteFile(string filePath);
     }
     public class UploadImage  : IUploadFile
     {
         private readonly IWebHostEnvironment host;
+        private string _baseFolder;
+        private string _baseFolderFullPath;
 
         public UploadImage(IWebHostEnvironment host)
         {
             this.host = host;
+            _baseFolder = "Resource";
+            _baseFolderFullPath = host.ContentRootPath;
         }
 
         public async Task<string> Upload(IFormFile file,string folderName)
@@ -27,25 +32,24 @@ namespace MeroBolee.Infrastructure
             string fileName;
             try
             {
-                string folder = "Resource";
                 if (file == null)
                 {
                     return null;
                 }
                 var extension = Path.GetExtension(file.FileName);// "." + file.FileName.Split('.')[file.FileName.Split('.').Length-1]; // to get extension of file
                 fileName = DateTime.Now.Ticks + extension; // to change name of file for security reason
-                var pathBuilt = Path.Combine(host.ContentRootPath, folder + "\\" + folderName); // to create path for file
+                var pathBuilt = Path.Combine(_baseFolderFullPath, _baseFolder + "\\" + folderName); // to create path for file
                 if (!Directory.Exists(pathBuilt))
                 {
                     Directory.CreateDirectory(pathBuilt);
                 }
-                var path = Path.Combine(host.ContentRootPath, folder + "\\" + folderName,fileName);
+                var path = Path.Combine(_baseFolderFullPath, _baseFolder + "\\" + folderName,fileName);
                 using (var Stream = new FileStream(path, FileMode.Create))
                 {
                     await file.CopyToAsync(Stream);
                 }
              //   isSaveSuccess = true;
-                return Path.Combine(folder, folderName, fileName);
+                return Path.Combine(_baseFolder, folderName, fileName);
             }
             catch (Exception)
             {
@@ -54,17 +58,33 @@ namespace MeroBolee.Infrastructure
         
         }
 
+        public Task<bool> DeleteFile(string filePath)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    string fullPath = Path.Combine(_baseFolderFullPath, filePath);
+                    File.Delete(fullPath);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            });
+        }
         //public async Task<IFormFile> Download(string filePath)
         //{
         //    try
         //    {
-               
+
         //    }
         //    catch (Exception)
         //    {
         //        throw; 
         //    }
-        
+
         //}
     }
 }
